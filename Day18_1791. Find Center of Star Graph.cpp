da44acfd1ca355f1316596e6c8a8e6975d36a735
Priyanshu <87303531+priyanshu-etc@users.noreply.github.com>
2024-06-28 23:50:46 +0530

    Update and rename Day18_ to Day18_1791. Find Center of Star Graph.cpp

---
+++ Day18_1791. Find Center of Star Graph.cpp
@@ -0,0 +1,16 @@
+// Code:
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        int n = edges.size() + 1;
+        
+        vector<int>& firstEdge = edges[0];
+        vector<int>& secondEdge = edges[1];
+        
+        if (firstEdge[0] == secondEdge[0] || firstEdge[0] == secondEdge[1]) {
+            return firstEdge[0];
+        } else {
+            return firstEdge[1];
+        }
+    }
+};
