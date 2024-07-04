72be550851ac7315e5f88544fb9d197fcaf8f4fb
Priyanshu <87303531+priyanshu-etc@users.noreply.github.com>
2024-07-05 02:56:09 +0530

    Update and rename Day25_ to Day25_2181. Merge Nodes in Between Zeros.cpp

---
+++ Day25_2181. Merge Nodes in Between Zeros.cpp
@@ -0,0 +1,27 @@
+// CODE:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   ListNode* mergeNodes(ListNode* head) {
+     head=head->next;
+     ListNode* start=head;
+     while(start){
+	    ListNode* end= start;   
+        int sum=0;
+        while(end->val!=0) sum+= end->val , end=end->next;
+        start->val=sum;   
+        start->next=end->next;  
+        start=start->next;   
+	 }
+     return head;
+}
+};
