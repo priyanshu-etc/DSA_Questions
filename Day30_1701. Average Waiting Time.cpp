7915a621f1210fe142b46db933fc26eb58166615
Priyanshu <87303531+priyanshu-etc@users.noreply.github.com>
2024-07-11 22:34:35 +0530

    Update and rename Day30_ to Day30_1701. Average Waiting Time.cpp

---
+++ Day30_1701. Average Waiting Time.cpp
@@ -0,0 +1,25 @@
+// CODE:
+class Solution {
+public:
+    double averageWaitingTime(vector<vector<int>>& customers) {
+
+        int currentTime = 0;
+        long long totalwaitTime = 0;
+
+        for (auto customer : customers) {
+
+            int arrival = customer[0];
+            int time = customer[1];
+
+            if (currentTime < arrival) {
+                currentTime = arrival;
+            }
+            int waitTime = currentTime + time - arrival;
+
+            totalwaitTime += waitTime;
+
+            currentTime += time;
+        }
+        return static_cast<double>(totalwaitTime) / customers.size();
+    }
+};
