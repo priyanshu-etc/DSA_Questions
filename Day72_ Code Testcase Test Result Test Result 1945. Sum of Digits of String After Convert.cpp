1b99298bef9e9c0bd67d370b33d0cfbfb12076ff
Priyanshu <87303531+priyanshu-etc@users.noreply.github.com>
2024-09-15 23:51:05 +0530

    Update and rename Day72_ to Day72_ Code Testcase Test Result Test Result 1945. Sum of Digits of String After Convert.cpp

---
+++ Day72_ Code Testcase Test Result Test Result 1945. Sum of Digits of String After Convert.cpp
@@ -0,0 +1,20 @@
+
+class Solution {
+public:
+    int getLucky(string s, int k) {
+        string number = "";
+        for (char x : s) {
+            number += to_string(x - 'a' + 1);
+        }
+        
+        while (k > 0) {
+            int temp = 0;
+            for (char x : number) {
+                temp += x - '0';  // Sum the digits of the current number
+            }
+            number = to_string(temp);  // Convert the sum back to a string
+            k--;
+        }
+        return stoi(number);  // Return the final result as an integer
+    }
+};
